fb5a4e9860310b6bf580d4e079cd558222e00b9e
RashidKhalique <rashidkhanjamali26@gmail.com>
2024-12-02 15:09:04 +0500

    modified role3

--- Routes/Hrexpensive.route.js
+++ Routes/Hrexpensive.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import role from "../Controllers/role.js"
+
 import categoriespost from "../Controllers/categorydatapost.js";
 import {findExpensesByDate, findExpensesByMonth,findExpensesByYear,findExpensesByweek} from "../Controllers/categorydataget.js"
 
@@ -11,6 +11,6 @@ Router.get('/hr-expensive/category/year/:id',findExpensesByYear)
 Router.get('/hr-expensive/category/month/:year/:month', findExpensesByMonth);
 Router.get('/hr-expensive/category/date/:dateparm', findExpensesByDate);
 Router.get('/hr-expensive/category/week',findExpensesByweek)
-Router.post('/role',role)
+
 
 export default Router;
